3bc86ee78ae3ea824149783bcc94b3a9e6e2df33
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2025-01-26 10:50:53 -0600

    init commit

---
+++ CodeChall/CodeChall/CodeChallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallApp.swift
+//  CodeChall
+//
+//  Created by Alan Emiliano Ramirez Ayala on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeChall/CodeChall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeChall
+//
+//  Created by Alan Emiliano Ramirez Ayala on 26/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
