3baec8dfa1a5dd93fd47516fca8458b742283042
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2025-01-26 17:09:30 -0600

    add loader and move groups

--- CodeChall/CodeChall/CodeChallApp.swift
+++ CodeChall/CodeChall/CodeChallApp.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 @main
 struct CodeChallApp: App {
+    @ObservedObject private var loaderState = GlobalState.shared
     var body: some Scene {
         WindowGroup {
-            CCHomeView()
+            ZStack {
+                CCHomeView()
+                if loaderState.isLoading {
+                    loadingView
+                }
+            }
         }
     }
+    
+    private var loadingView: some View {
+        ZStack {
+            Color.black.opacity(0.5)
+                .ignoresSafeArea()
+            VStack {
+                ProgressView()
+                    .scaleEffect(1.5)
+                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                Text("Cargando...")
+                    .foregroundStyle(.white)
+                    .padding(.top, 10)
+            }
+        }
+        .transition(.opacity)
+        .animation(.easeInOut, value: loaderState.isLoading)
+    }
+}
+
+class GlobalState: ObservableObject {
+    static var shared: GlobalState = GlobalState()
+    @Published var isLoading: Bool = false
 }
--- CodeChall/CodeChall/Services/CCSendService.swift
+++ CodeChall/CodeChall/Services/CCSendService.swift
@@ -13,19 +13,23 @@ final class CCSendService {
         urlRequest: URLRequest,
         responseType: T.Type
     ) async throws -> T {
+        GlobalState.shared.isLoading.toggle()
         let request = urlRequest
         print("Request: \(request.prettyPrinted())")
         let (data, response) = try await URLSession.shared.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
             print("Error en respuesta: \(response)")
+            GlobalState.shared.isLoading.toggle()
             throw CCAPIError.responseUnsuccessful
         }
         let decoder = JSONDecoder()
         do {
             let decodedResponse = try decoder.decode(T.self, from: data)
+            GlobalState.shared.isLoading.toggle()
             return decodedResponse
         } catch {
             print("Error al decodificar la respuesta: \(error)")
+            GlobalState.shared.isLoading.toggle()
             throw CCAPIError.jsonConversionFailure
         }
     }
