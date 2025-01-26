af41611fe8e8bab4a7491f65f188b4dac7a84dcc
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2025-01-26 17:29:17 -0600

    add list and search action

--- CodeChall/CodeChall/Modules/CCHome/View/CCHomeView.swift
+++ CodeChall/CodeChall/Modules/CCHome/View/CCHomeView.swift
@@ -10,7 +10,29 @@ import SwiftUI
 struct CCHomeView: View {
     @ObservedObject var viewModel: CCHomeViewModel = CCHomeViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            listView
+        }
+        .searchable(text: $viewModel.strSearchText, prompt: "Buscar")
+        .onChange(of: viewModel.strSearchText) { _, newValue in
+            if !newValue.isEmpty {
+                viewModel.searchData(dataName: newValue)
+            } else {
+                viewModel.emptyValueSearch()
+            }
+        }
+    }
+    
+    private var listView: some View {
+        List(viewModel.searchData, id: \.id) { crypto in
+            Text(crypto.name)
+        }
+    }
+    
+    private var rowCrypto: some View {
+        VStack {
+            
+        }
     }
 }
 
--- CodeChall/CodeChall/Modules/CCHome/ViewModel/CCHomeViewModel.swift
+++ CodeChall/CodeChall/Modules/CCHome/ViewModel/CCHomeViewModel.swift
@@ -13,12 +13,26 @@ class CCHomeViewModel: ObservableObject {
         selfClass.output = self
         return selfClass
     }
-    private var data: CCCryptoData?
+    private var data: [CCCryptoDatum] = []
+    @Published var strSearchText: String = ""
+    @Published var searchData: [CCCryptoDatum] = []
     
     init() {
         Task {
             await remoteDataManager.getData()
         }
+    }
+    
+    func getCryptoData() -> CCCryptoData? {
+        return data
+    }
+    
+    func searchData(dataName: String) {
+        searchData = data.filter {$0.name.lowercased().contains(dataName.lowercased())}
+    }
+    
+    func emptyValueSearch() {
+        searchData = Array(data.prefix(20))
     }
    
 }
@@ -26,6 +40,7 @@ class CCHomeViewModel: ObservableObject {
 extension CCHomeViewModel: CCHomeRemoteDataManagerOutput {
     func successResponse(data: CCCryptoData) {
         self.data = data
+        emptyValueSearch()
     }
     
     func errorResponse(error: any Error) {
