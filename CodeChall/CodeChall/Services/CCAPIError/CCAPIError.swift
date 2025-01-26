a065940e34522e116583f542b1ca64049d75e189
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2025-01-26 10:53:53 -0600

    Add enum error

---
+++ CodeChall/CodeChall/Services/CCAPIError/CCAPIError.swift
@@ -0,0 +1,26 @@
+//
+//  CCApiError.swift
+//  CodeChall
+//
+//  Created by Alan Emiliano Ramirez Ayala on 26/01/25.
+//
+
+import Foundation
+
+enum CCAPIError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case jsonParsingFailure
+    
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed: return "Request Failed"
+        case .invalidData: return "Invalid Data"
+        case .responseUnsuccessful: return "Response Unsuccessful"
+        case .jsonParsingFailure: return "JSON Parsing Failure"
+        case .jsonConversionFailure: return "JSON Conversion Failure"
+        }
+    }
+}
